ae49bb1f9ca9af0601ab32ef51e6754354f1b241
Emma Martí <emmardi@gmail.com>
2023-10-25 18:06:27 +0200

    Add project files.

---
+++ LibraryEmd/Operation.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryEmd
+{
+    public class Operation
+    {
+        public int AddNumbers(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+    }
+}
---
+++ LibraryEmdMSTest/OperationMSTest.cs
@@ -0,0 +1,31 @@
+﻿using LibraryEmd;
+
+namespace LibraryEmdMSTest
+{
+    [TestClass]
+    public class OperationMSTest
+    {
+        [TestMethod]
+
+        public void AddNumbers_InputTwoNumbers_GetCorrectValue()
+        {
+            //1.Arrange
+            //Initialize the variables or components that execute the test
+
+            Operation op = new();
+            int number1Test = 50;
+            int number2Test = 69;
+
+            //2.Act
+            //Execute de Operation
+
+            int result = op.AddNumbers(number1Test, number2Test);
+
+            //3.Assert
+            //Compare the result. Unittest Library
+
+            Assert.AreEqual(119, result);
+
+        }
+    }
+}
